f6a559e6d06d54a606599245524c9047fc5a8c78
niyari <niyari@users.noreply.github.com>
2021-05-17 01:41:12 +0900

    Change Options.

--- dist/base32.d.ts
+++ dist/base32.d.ts
@@ -1,25 +1,22 @@
 /*! github.com/niyari/base32-ts/ MIT */
 declare type Variant = '3548' | '4648' | 'hex' | 'clockwork' | 'maki' | 'wah' | 'crockford' | '';
+declare type CrockFordEncoderInput = bigint | Number;
+declare type MultiEncoderInput = Uint8Array | string;
 interface Base32Options {
     variant?: Variant;
     padding?: boolean;
     array?: boolean;
-}
-interface CrockFordEncoderOptions {
+    raw?: boolean;
     split?: number;
     checksum?: boolean;
 }
-interface CrockFordDecoderOptions {
-    checksum?: boolean;
-    raw?: boolean;
-}
-interface LastErrorTypes {
+interface ErrorArray {
     isError: boolean;
     message: string;
 }
 interface ReturnArray {
     data: string | ArrayBuffer;
-    error?: LastErrorTypes;
+    error?: ErrorArray;
 }
 export declare class Base32 {
     private _mode;
@@ -33,8 +30,8 @@ export declare class Base32 {
     private returnArray;
     private setError;
     private resetError;
-    encode: ((input: bigint | Number, options?: CrockFordEncoderOptions) => string | ReturnArray) | ((input: Uint8Array | string) => string | ReturnArray);
-    decode: (input?: string, options?: CrockFordDecoderOptions) => string | ArrayBuffer | ReturnArray;
-    lasterror(): LastErrorTypes;
+    encode(input: MultiEncoderInput | CrockFordEncoderInput): string | ReturnArray;
+    decode(input: string): string | ArrayBuffer | ReturnArray;
+    lasterror(): ErrorArray;
 }
 export {};
--- test/benchmark.js
+++ test/benchmark.js
@@ -1,6 +1,6 @@
 import { Base32 as B } from "../dist/base32.js";
 (() => {
-    const benchmarkCount = 1000000;
+    const bCount = 1000000;
     let _sT = 0;
     const bStart = (text) => {
         console.log("\u001b[43;30m >>>>> " + text + " \u001b[0m");
@@ -8,8 +8,8 @@ import { Base32 as B } from "../dist/base32.js";
     }
     const bEnd = () => {
         let endT = performance.now();
-        console.log(" \u001b[42m🕒 \u001b[0m\u001b[32m " + benchmarkCount + " count\u001b[0m",
-            "\u001b[90m" + (endT - _sT).toPrecision(4) + "ms\u001b[0m ", Math.round(benchmarkCount / (endT - _sT) * 1000 * 1000) / 1000 + " per second.\n");
+        console.log(" \u001b[42m🕒 \u001b[0m\u001b[32m " + bCount + " count\u001b[0m",
+            "\u001b[90m" + (endT - _sT).toPrecision(4) + "ms\u001b[0m ", Math.round(bCount / (endT - _sT) * 1000 * 1000) / 1000 + " per second.\n");
     }
     try {
 
@@ -21,57 +21,69 @@ import { Base32 as B } from "../dist/base32.js";
         const rnd = () => { return Math.random().toString(16) }
 
         bStart('base32.encode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             b32.encode(rnd());
         }
         bEnd();
         bStart('base32.encode -> decode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             b32.decode(b32.encode(rnd()));
         }
         bEnd();
 
         bStart('base32_clockwork.encode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             clkw.encode(rnd());
         }
         bEnd();
         bStart('base32_clockwork.encode -> decode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             clkw.decode(clkw.encode(rnd()));
         }
         bEnd();
 
         bStart('base32_crockford.encode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             crf.encode(i);
         }
         bEnd();
         bStart('base32_crockford.encode -> decode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             crf.decode(crf.encode(i));
         }
         bEnd();
 
         bStart('base32_arr.encode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             b32_arr.encode(rnd());
         }
         bEnd();
         bStart('base32.encode -> base32_arr.decode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             b32_arr.decode(b32.encode(rnd()));
         }
         bEnd();
 
         bStart('base32_crockford_arr.encode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             crf_arr.encode(i ^ 2);
         }
         bEnd();
         bStart('base32_crockford.encode -> decode');
-        for (let i = 0; i < benchmarkCount; i++) {
+        for (let i = 0; i < bCount; i++) {
             crf_arr.decode(crf.encode(i ^ 2));
+        }
+        bEnd();
+
+        //implementation standard of the node.js
+        bStart('nodejs:ascii to base64 encode');
+        for (let i = 0; i < bCount; i++) {
+            Buffer.from(rnd(), 'ascii').toString('base64');
+        }
+        bEnd();
+        bStart('nodejs:ascii to base64 encode -> decode');
+        for (let i = 0; i < bCount; i++) {
+            Buffer.from(Buffer.from(rnd(), 'ascii').toString('base64'), 'base64').toString('ascii');
         }
         bEnd();
 
